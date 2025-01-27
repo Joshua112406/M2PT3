92908774b788eeb35f080dc4d7db7814b7fbe862
Joshua112406 <liawjoshua33@gmail.com>
2025-01-27 21:39:45 +0800

    Add files via upload

---
+++ Part1.java
@@ -0,0 +1,13 @@
+public class Part1 {
+    public static void main(String[] args) {
+        String name = "Joshua Jiawei E. Liaw";
+        int age = 18;
+        double height = 1.75;
+        boolean isStudent = true;
+ 
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height + " meters");
+        System.out.println("Is a Student? " + isStudent);
+    }
+}
